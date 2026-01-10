d12a7b6378fbfdb43eb8b5fd75ee204fcd96e0dc
Michael Sun <mich536ael@gmail.com>
2026-01-10 14:27:15 -0800

    fix json, sql

--- reporting-functions/circ_stats_course_reserves_ALL.sql
+++ reporting-functions/circ_stats_course_reserves_ALL.sql
@@ -18,14 +18,14 @@ AS $$
 SELECT 
     iext.barcode AS item_barcode,
     inst.title AS instance_title,
-    crct.course_number AS course_number,
+    crct.coursenumber AS course_number,
     COALESCE(lit.clid, 0) AS circ_count
 FROM
-    folio_courses.coursereserves_courses__t__ crct
-LEFT JOIN folio_courses.coursereserves_reserves__t__ crrt
-       ON crct.course_listing_id = crrt.course_listing_id
+    folio_courses.coursereserves_courses__t crct
+LEFT JOIN folio_courses.coursereserves_reserves__t crrt
+       ON crct.courselistingid = crrt.courselistingid
 LEFT JOIN folio_derived.item_ext iext
-       ON crrt.item_id = iext.item_id
+       ON crrt.itemid = iext.item_id
 LEFT JOIN folio_derived.holdings_ext hrt
        ON iext.holdings_record_id = hrt.holdings_id
 LEFT JOIN folio_derived.instance_ext inst
@@ -33,33 +33,33 @@ LEFT JOIN folio_derived.instance_ext inst
 LEFT JOIN (
         SELECT 
             item_id,
-            COUNT(loan_id) AS clid -- Changed back to loan_id
+            COUNT(loan_id) AS clid
         FROM folio_derived.loans_items
         WHERE 
-            (loan_date::date >= start_date OR start_date IS NULL)
-            AND (loan_date::date <= end_date OR end_date IS NULL)
+            loan_date::date >= start_date
+            AND loan_date::date <= end_date
         GROUP BY item_id
 ) lit
-       ON lit.item_id = crrt.item_id
+       ON lit.item_id = crrt.itemid
 WHERE 
-    crrt.item_id IS NOT NULL
+    crrt.itemid IS NOT NULL
     -- Exclusion Logic
     AND (
         exclusions IS NULL OR (
-            (NOT (exclusions ILIKE '%POP%') OR (crct.course_number IS DISTINCT FROM 'POP')) AND
-            (NOT (exclusions ILIKE '%LAW%') OR (crct.course_number NOT ILIKE 'Law' AND crct.course_number NOT ILIKE 'LAW')) AND
-            (NOT (exclusions ILIKE '%NEW%') OR (crct.course_number IS DISTINCT FROM 'NEW')) AND
-            (NOT (exclusions ILIKE '%EMPTY%') OR (crct.course_number IS NOT NULL AND crct.course_number <> ''))
+            (exclusions NOT ILIKE '%POP%' OR crct.coursenumber IS DISTINCT FROM 'POP') AND
+            (exclusions NOT ILIKE '%LAW%' OR (crct.coursenumber NOT ILIKE 'Law' AND crct.coursenumber NOT ILIKE 'LAW')) AND
+            (exclusions NOT ILIKE '%NEW%' OR crct.coursenumber IS DISTINCT FROM 'NEW') AND
+            (exclusions NOT ILIKE '%EMPTY%' OR (crct.coursenumber IS NOT NULL AND crct.coursenumber <> ''))
         )
     )
     -- Course Code Logic
     AND (
         course_codes IS NULL 
         OR course_codes = ''
-        OR crct.course_number = ANY(string_to_array(replace(course_codes, ', ', ','), ','))
+        OR crct.coursenumber = ANY(string_to_array(course_codes, ','))
     )
 GROUP BY 
-    iext.barcode, inst.title, crct.course_number, lit.clid
+    iext.barcode, inst.title, crct.coursenumber, lit.clid
 $$
 LANGUAGE sql
 STABLE
